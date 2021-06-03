4908a7e8e338b3c85c4e2342087e0567b2545655
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-06-03 18:20:26 -0300

    little code view adjusts

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-/*
-				JSX code format (JS + XML)
-				But it can be converted to JS code using the React.div library.
-
-
-	------------ JSX Sample: ------------
-
-	<div>
-		<h1>Hii</h1>
-		<a href="google.com">click here</a>
-	</div>
-
-	------------ JS with React lib Sample: ------------
-
-	React.createElement(
-		'div',
-		null,
-		{ id: 'some-div' },
-		React.createElement(
-			'h1',
-			null,
-			'Hii'
-		),
-		React.createElement(
-			'a',
-			{ href: 'google.com' },
-			'click here'
-		)
-	)
-
-*/
-
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { PageBody } from "./pages/home.js"
@@ -63,3 +31,35 @@ function App() {
 
 
 export { InitLayout, App }
+
+/*
+				JSX code format (JS + XML)
+				But it can be converted to JS code using the React.div library.
+
+
+	------------ JSX Sample: ------------
+
+	<div>
+		<h1>Hii</h1>
+		<a href="google.com">click here</a>
+	</div>
+
+	------------ JS with React lib Sample: ------------
+
+	React.createElement(
+		'div',
+		null,
+		{ id: 'some-div' },
+		React.createElement(
+			'h1',
+			null,
+			'Hii'
+		),
+		React.createElement(
+			'a',
+			{ href: 'google.com' },
+			'click here'
+		)
+	)
+
+*/
